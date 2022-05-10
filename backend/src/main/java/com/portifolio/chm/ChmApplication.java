40f75e536116a90725c09bab7775eea136ae7bef
claudiohmbr <104603152+claudiohmbr@users.noreply.github.com>
2022-05-10 01:22:00 -0300

    primeiro commit

---
+++ backend/src/main/java/com/portifolio/chm/ChmApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.chm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/portifolio/chm/ChmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.chm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
