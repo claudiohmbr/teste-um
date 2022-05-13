da366f0f25bbaf130198990bd91e65069ed0865a
Claudio Machado <104603152+claudiohmbr@users.noreply.github.com>
2022-05-13 10:03:45 -0300

    Navigation Button

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,37 @@
-import Pagination from "components/navbar/Pagination"
 
+import Pagination from "components/Pagination"
+
+import MovieCard from "components/MovieCard"
 function Listing() {
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
+           { /*Colunas dos cards*/}
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col sl-3 mb-3">
+                       <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col sl-3 mb-3">
+                       <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col sl-3 mb-3">
+                       <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col sl-3 mb-3">
+                       <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col sl-3 mb-3">
+                       <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col sl-3 mb-3">
+                       <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+            
+        </>
     )
 }
 
